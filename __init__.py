5f821810f0370dca9a3c5a452568a61b75d11f5c
Kate Belous <44820191+katebeloussmile@users.noreply.github.com>
2021-12-04 21:09:53 +0200

    4

--- __init__.py
+++ __init__.py
@@ -508,4 +508,4 @@ if __name__ == '__main__':
     # app.secret_key = 'super_secret_key'
     # app.debug = True
     port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False)
