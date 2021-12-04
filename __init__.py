17e1f6e3ae804ce0e9d22f73158932288e886052
Kate Belous <44820191+katebeloussmile@users.noreply.github.com>
2021-12-04 20:34:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -507,4 +507,4 @@ def deleteMenuItem(restaurant_id, menu_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
